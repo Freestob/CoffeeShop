d4e2d3188477bd3e125fb36e63e269389434e00a
Bradley Freestone <bradley.freestone@gmail.com>
2018-08-14 18:18:06 -0400

    first commit

---
+++ CoffeeShopApplicationLab/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CoffeeShopApplicationLab.MvcApplication" Language="C#" %>
---
+++ CoffeeShopApplicationLab/Models/Registerer.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace CoffeeShopApplicationLab.Models
+{
+    public class Registerer
+    {
+
+        [MinLength(3)]
+        [Required]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "That is not a valid name")]
+        public string FirstName { get; set; }
+
+
+        [MinLength(3)]
+        [Required]
+        [RegularExpression("^[a-zA-Z]+$", ErrorMessage = "That is not a valid name")]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [RegularExpressionAttribute(@"^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^\da-zA-Z]).{8,15}$", ErrorMessage = "Password must contain one uppercase, one lowercase, one number, and one special character")]
+        public string Password { get; set; }
+
+    }
+}
